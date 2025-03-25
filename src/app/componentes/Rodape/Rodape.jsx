748d0443062924b0636b626f3c5cf15d1152ff33
lipenspereira39 <156716339+lipenspereira39@users.noreply.github.com>
2025-03-25 11:59:11 -0300

    Add files via upload

---
+++ src/app/componentes/Rodape/Rodape.jsx
@@ -0,0 +1,37 @@
+import estilos from './Rodape.module.css';
+import Image from 'next/image';
+import Logo from '/public/Logo2.png';
+import iconeFacebook from '/public/facebook.png';
+import iconeTwitter from '/public/twitter.png';
+import iconeLinkedin from '/public/linkedin.png';
+import iconeDribble from '/public/dribble.png';
+import iconeBehance from '/public/behance.png';
+import iconeGooglePlus from '/public/google.png';
+
+export default function Rodape(props) {
+
+  return(
+    <footer className={props.ehTemaEscuro ? estilos.rodape_modo_escuro : estilos.rodape_modo_claro}>
+      <div className={estilos.container_informacoes}>
+        <div>
+          <Image src={Logo} height={80} width={80} alt="logomarca"/>
+          <p className={estilos.paragrafo}>
+            Ajudamos a criar uma personalidade digital construindo sua marca no ambiente online utilizando estratégias,
+            ferramentas e tecnologias personalizadas.
+          </p>
+        </div>
+        <div>
+          <Image src={iconeFacebook} alt="facebook"/>
+          <Image src={iconeTwitter} alt="twitter"/>
+          <Image src={iconeLinkedin} alt="linkedin"/>
+          <Image src={iconeDribble} alt="dribble"/>
+          <Image src={iconeBehance} alt="behance"/>
+          <Image src={iconeGooglePlus} alt="google plus"/>
+        </div>
+      </div>
+      <div className={estilos.copyright}>
+        <p>Copyright 2025 &copy; <span>Filipi Pereira</span></p>
+      </div>
+    </footer>
+  )
+}
---
+++ src/app/componentes/SecaoBanner/SecaoBanner.jsx
@@ -0,0 +1,14 @@
+import estilos from './SecaoBanner.module.css';
+
+export default function SecaoBanner(props) {
+  return(
+    <section className={estilos.secao_banner}>
+      <div className={props.ehTemaEscuro ? estilos.imagemDeFundo_modo_escuro : estilos.imagemDeFundo_modo_claro}></div>
+      <div className={estilos.texto_secaoBanner}>
+        <p>BRANDING / UI / UX / TECNOLOGIA</p>
+        <h2>Agência de Branding</h2>
+        <span>e design digital</span>
+      </div>
+    </section>
+  )
+}
